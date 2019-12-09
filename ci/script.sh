3fb8221c9d3f85da19eb5c4b1a2007ebe2b6cfc2
Sami Salonen <ssalonen@gmail.com>
2019-12-09 21:05:30 +0200

    CI: Test that example builds
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- ci/script.sh
+++ ci/script.sh
@@ -14,9 +14,13 @@ main() {
     cross test --target $TARGET
     cross test --target $TARGET --release
 
-    # no binaries, nothing to run
-    # cross run --target $TARGET
-    # cross run --target $TARGET --release
+    # test that example builds
+    for example in $(ls examples/); do
+        (
+            cd $example
+            cross check --target $TARGET
+        )
+    done
 }
 
 # we don't run the "test phase" when doing deploys
