0237140004cb85a0e190447b4624855359ea01bd
Sami Salonen <ssalonen@gmail.com>
2019-12-09 21:48:24 +0200

    CI: workaround cross restrictions
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- ci/script.sh
+++ ci/script.sh
@@ -14,9 +14,23 @@ main() {
     cross test --target $TARGET
     cross test --target $TARGET --release
 
+    #
     # test that example builds
-    for example in $(ls examples/); do
-        ( cd examples/$example && cross check --target $TARGET)        
+    #
+    CEC_RS_EXAMPLE_BUILD=$(mktemp -d -t cec-rs-example-build-XXXXXXXXX)
+    function cleanup {
+        rm -rf $CEC_RS_EXAMPLE_BUILD
+    }
+    trap cleanup EXIT
+    echo "Building cec-rs example in $CEC_RS_EXAMPLE_BUILD"    
+    for example in $(ls examples/); do        
+        # Workaround Cross caveat of path dependencies pointing outside Cargo project
+        # by copying sources to temporary directory
+        cp -a examples/$example $CEC_RS_EXAMPLE_BUILD/$example
+        mkdir $CEC_RS_EXAMPLE_BUILD/$example/cec_rs_src && cp -a {src,Cargo.toml,Cross.toml} $CEC_RS_EXAMPLE_BUILD/$example/cec_rs_src
+        sed -i -E 's@cec-rs\s*=\s*\{\s*path\s*=\s*"../.."\s*\}@cec-rs = { path = "cec_rs_src" }@' $CEC_RS_EXAMPLE_BUILD/$example/Cargo.toml
+        ( cd $CEC_RS_EXAMPLE_BUILD/$example/ \
+          && cross check --target $TARGET )        
     done
 }
 
